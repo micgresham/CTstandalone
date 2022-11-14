ec144e7b4bd4f160f80f827723c4df196c0a10a4
Michael Gresham <michael.gresham@hpe.com>
2022-11-14 10:18:30 -0500

    no message

--- CTadd_var/makebin.sh
+++ CTadd_var/makebin.sh
@@ -1,10 +1,10 @@
  
 # macos
-GOOS=darwin GOARCH=amd64 go build -o bin/$1-macos $1.go
+GOOS=darwin GOARCH=amd64 go build -o ../bin/$1-macos $1.go
 
 # windows 
-GOOS=windows GOARCH=amd64 go build -o bin/$1-64.exe $1.go
-GOOS=windows GOARCH=386 go build -o bin/$1-32.exe $1.go
+GOOS=windows GOARCH=amd64 go build -o ../bin/$1-64.exe $1.go
+GOOS=windows GOARCH=386 go build -o ../bin/$1-32.exe $1.go
 
 # linux 
-GOOS=linux GOARCH=amd64 go build -o bin/$1-linux $1.go
+GOOS=linux GOARCH=amd64 go build -o ../bin/$1-linux $1.go
