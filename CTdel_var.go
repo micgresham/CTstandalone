e8dfbe2c7e9bf713b3a4d6a31efe6b2fc2604cbe
Michael Gresham <>
2023-04-10 17:59:25 -0400

    Added -c option to specify the location of the config YML file

---
+++ CTdel_var.go
@@ -0,0 +1,463 @@
+package main
+
+
+import (
+    "fmt"
+    "os"
+    "io/ioutil"
+    "io"
+    "bytes"
+    "bufio"
+    "mime/multipart"
+    "net/http"
+    "time"
+    "log"
+    "strings"
+    "github.com/buger/jsonparser"
+    "github.com/akamensky/argparse"
+    "sigs.k8s.io/yaml"
+    "github.com/micgresham/goCentral"
+)
+
+var appName = "CTdel_var"
+var appVer = "1.5"
+var appAuthor = "Michael Gresham"
+var appAuthorEmail = "michael.gresham@hpe.com"
+var pgmDescription = fmt.Sprintf("%s: Delete a variable in Central for a list of device serial numbers.",appName)
+var central_info goCentral.Central_struct
+var useSecureStorage = true
+
+var p_check_dict = map[string]interface{}{}
+
+func get_variables(central_info goCentral.Central_struct, serial string) string {
+
+  access_token := central_info.Token
+  base_url := central_info.Base_url
+  api_function_url := fmt.Sprintf("%sconfiguration/v1/devices/%s/template_variables",base_url,serial)
+
+  c := http.Client{Timeout: time.Duration(10) * time.Second}
+  req, err := http.NewRequest("GET", api_function_url, nil)
+  if err != nil {
+      fmt.Printf("error %s", err)
+      return("")
+  }
+  q := req.URL.Query()
+  q.Add("device_serial",serial)
+  req.URL.RawQuery = q.Encode()
+
+  req.Header.Add("Content-Type", `application/json`)
+  req.Header.Add("Authorization", fmt.Sprintf("Bearer %s",fmt.Sprintf(access_token)))
+  resp, err := c.Do(req)
+  if err != nil {
+      fmt.Printf("error %s", err)
+      return("")
+  }
+
+  defer resp.Body.Close()
+  body, err := ioutil.ReadAll(resp.Body)
+//  fmt.Printf("%s",body)
+//  fmt.Printf("**************\n")
+  _sys_serial, err := jsonparser.GetString(body, "data", "variables", "_sys_serial")
+  if err != nil {
+      fmt.Printf("error %s", err)
+      return("")
+  }
+
+  data, _, _, err := jsonparser.Get(body, "data")
+  if err != nil {
+      fmt.Printf("error %s", err)
+      return("")
+  }
+  data_dict := fmt.Sprintf("{ \"%s\" : %s }",_sys_serial,data)
+
+  return(string(data_dict))
+}
+
+func createMultipartFormData(fileFieldName, filePath string, fileName string, extraFormFields map[string]string) (b bytes.Buffer, w *multipart.Writer, err error) {
+  w = multipart.NewWriter(&b)
+  var fw io.Writer
+  file, err := os.Open(filePath)
+
+  if fw, err = w.CreateFormFile(fileFieldName, fileName); err != nil {
+      return
+  }
+  if _, err = io.Copy(fw, file); err != nil {
+      return
+  }
+
+  for k, v := range extraFormFields {
+      w.WriteField(k, v)
+  }
+
+  w.Close()
+
+  return
+}
+
+func del_variables(central_info goCentral.Central_struct, fname string) string {
+
+  access_token := central_info.Token
+  base_url := central_info.Base_url
+  api_function_url := fmt.Sprintf("%s/configuration/v1/devices/template_variables",base_url)
+
+  extraFields := map[string]string{}
+  data, w, err := createMultipartFormData("variables",fname,fname, extraFields )
+    if err != nil {
+       return("ERROR Create Multipart Form Data")
+    }
+
+  c := http.Client{Timeout: time.Duration(10) * time.Second}
+  req, err := http.NewRequest("PUT", api_function_url, &data)
+  if err != nil {
+      fmt.Printf("error %s", err)
+      return("ERROR")
+  }
+  q := req.URL.Query()
+  req.URL.RawQuery = q.Encode()
+
+  req.Header.Set("Content-Type", w.FormDataContentType())
+  req.Header.Add("Authorization", fmt.Sprintf("Bearer %s",fmt.Sprintf(access_token)))
+  resp, err := c.Do(req)
+  if err != nil {
+      fmt.Printf("error %s", err)
+      return("")
+  }
+
+  defer resp.Body.Close()
+//  body, err := ioutil.ReadAll(resp.Body)
+//  fmt.Println(string(body))
+  
+  return(resp.Status)
+}
+
+
+
+func p_check(central_info goCentral.Central_struct, fname string, oprefix string, variable string) {
+
+  f := func() *os.File {
+     f, err := os.Open(fname)
+     if err != nil {
+       panic(err)
+     }
+     return f
+  }()
+  fileScanner := bufio.NewScanner(f)
+  fileScanner.Split(bufio.ScanLines)
+  
+  var fileLines []string
+
+  for fileScanner.Scan() {
+    fileLines = append(fileLines, fileScanner.Text())
+  }
+
+  f.Close()
+
+  ofilename := fmt.Sprintf("%s-%s",oprefix,fname)
+  of, err := os.Create(ofilename)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+  for _, line := range fileLines {
+//        fmt.Println(line)
+        return_dict := []byte(get_variables(central_info, strings.TrimSpace(line)))
+//        fmt.Println(string(return_dict))
+        del_dict, _, _, err := jsonparser.Get(jsonparser.Delete(return_dict, line,"variables",variable), line, "variables")
+        if err != nil {
+          fmt.Printf("error %s", err)
+          return
+        }
+//        fmt.Println(string(del_dict))
+         
+        tmp_json := fmt.Sprintf("%s",string(del_dict))
+//        fmt.Println(string(tmp_json))
+        p_check_dict[line] = tmp_json 
+
+        yaml_vars, err := yaml.JSONToYAML(return_dict)
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+		return
+	}
+        n, err := of.WriteString(string(yaml_vars))
+        if err != nil {
+          fmt.Println(err)
+          return
+        }
+        if n == 0 {
+          fmt.Println("No bytes written")
+          return
+        }
+  } 
+  err = of.Close()
+   if err != nil {
+       fmt.Println(err)
+       return
+  }
+}
+
+func doesFileExist(fileName string) bool {
+   _ , error := os.Stat(fileName)
+
+// check if error is "file not exists"
+   if os.IsNotExist(error) {
+//     fmt.Printf("%v file does not exist\n", fileName)
+     return true
+   } else {
+//     fmt.Printf("%v file exist\n", fileName)
+     return false
+   }
+}
+
+func main() {
+
+  parser := argparse.NewParser(appName,pgmDescription)
+  token := parser.String("","token", &argparse.Options{Help: "Central API token if not using encrypted storage."})
+  base_url := parser.String("","url", &argparse.Options{Help: "Central API URL if not using encrypted storage."})
+  initDB := parser.Flag("","initDB", &argparse.Options{Help: "Initialize secure storage"})
+
+  infile := parser.String("","infile", &argparse.Options{Help: "Input file consisting of a single device serial on each line"})
+  variable := parser.String("","variable", &argparse.Options{Help: "Variable to delete"})
+  test := parser.Flag("t", "test", &argparse.Options{Help: "Enable test mode. No variables will be changed"})
+  config := parser.String("c", "config", &argparse.Options{Help: "Config file location"})
+
+  //encrypted storage setup
+  SSfilename:= "CTconfig.yml"
+
+  goCentral.Passphrase = "“You can use logic to justify almost anything. That’s its power. And its flaw. –Captain Cathryn Janeway"
+
+  fmt.Println("-------------------------------------")
+  fmt.Printf("%s Version: %s\r\n",appName, appVer)
+  fmt.Printf("Author: %s (%s)\r\n",appAuthor, appAuthorEmail)
+  fmt.Println("-------------------------------------")
+
+  err := parser.Parse(os.Args)
+  if err != nil {
+	fmt.Println(parser.Usage(err))
+	return
+  }
+
+  if *test {
+    fmt.Println("--------------------------------------------------")
+    fmt.Println("TEST MODE - NO VARIABLE CHANGE WILL BE PERFORMED")
+    fmt.Println("--------------------------------------------------")
+  }
+
+  if *config != "" {
+    SSfilename = *config
+    fmt.Println("-------------------------------------")
+    fmt.Println("Loading config file from ",SSfilename)
+    fmt.Println("-------------------------------------")
+  }
+
+  //initialize the secure storage if requested
+  if *initDB {
+     goCentral.Init_DB(SSfilename)
+     os.Exit(0) //we do not do anything after the secure storage initialization
+  }
+
+  if doesFileExist(SSfilename) {
+
+     //we are not using secure storage
+     useSecureStorage = false
+
+     //if the user provided a token AND a URL we will use it
+     if (*token != "") {
+        if (*base_url == "") {
+           fmt.Println("Token supplied, but Central URL is missing.  Both are required if using the command line options.")
+           os.Exit(1)
+        }
+     } else { //ask for the token
+
+     fmt.Print("\nProvide the Central API URL: ")
+     fmt.Scanln(base_url)
+     fmt.Print("Provide the Central token: ")
+     fmt.Scanln(token)
+
+     central_info.Base_url = *base_url
+     central_info.Customer_id = ""
+     central_info.Client_id = ""
+     central_info.Client_secret = ""
+     central_info.Token = *token
+     central_info.Refresh_token = ""
+
+     }
+  } else {
+    fmt.Println("Reading secure storage")
+
+    central_info = goCentral.Read_DB(SSfilename)
+    fmt.Printf("---------------------------\n")
+    fmt.Printf("Central Info Decrypted\n")
+    fmt.Printf("---------------------------\n")
+    fmt.Printf("Central URL: %s\n",central_info.Base_url)
+//    fmt.Printf("Central Customer ID: %s\n",central_info.Customer_id)
+//    fmt.Printf("Central Client ID: %s\n",central_info.Client_id)
+//    fmt.Printf("Central Client Secret: %s\n",central_info.Client_secret)
+    fmt.Printf("Central Token: %s\n",central_info.Token)
+//    fmt.Printf("Central Refresh Token: %s\n",central_info.Refresh_token)
+  }
+
+ if doesFileExist(*infile) {
+    fmt.Print("\nProvide the input file name: ")
+    fmt.Scanln(infile)
+ }
+
+ if (*variable == "") {
+   fmt.Print("\nProvide the Central variable to be deleted: ")
+   fmt.Scanln(variable)
+ }
+
+  fmt.Println("Input file name:",*infile)
+  fmt.Println("Variable to be deleted:",*variable)
+
+//======================================================
+// test if valid token
+//======================================================
+  respCode, new_token, new_refresh_token := goCentral.Test_central(central_info)
+  if (respCode != 200) {
+     fmt.Printf("\nCentral access failed with response code: %d\n",respCode)
+     os.Exit(3)
+  } else {
+     fmt.Print("Central access OK.  Token verified.")
+     fmt.Printf("Response code: %d\n",respCode)
+     central_info.Token = new_token
+     if useSecureStorage {
+       central_info.Refresh_token = new_refresh_token
+       goCentral.Write_DB(SSfilename,central_info)
+     }
+  }
+  if (respCode != 200) {
+   fmt.Printf("\nCentral access failed with response code: %d\n",respCode)
+   os.Exit(3)
+  }
+
+//======================================================
+// perform pre check
+//======================================================
+  fmt.Println("--------------------------------")
+  fmt.Println("Performing Pre-Check")
+  fmt.Println("--------------------------------")
+  p_check(central_info,*infile,"pre-check",*variable)
+
+
+//======================================================
+// perform variable deletion 
+//======================================================
+
+  fmt.Println("--------------------------------")
+  if *test {
+    fmt.Println("TEST MODE - NO VARIABLE CHANGE")
+  } else {
+    fmt.Println("Performing variable add/update")
+  }
+  fmt.Println("--------------------------------")
+
+  count := 0
+  count_batches := 0  
+  temp_string := ""
+  temp_dict := map[interface{}]interface{}{}
+  
+  for key, element := range p_check_dict {
+
+    fmt.Println("Setting device",key, "=>", "Values:", element)
+    temp_dict[key] = element  
+
+    if count == 250 {
+      fmt.Println("STOP, Hammer time")
+
+      //create temp file
+      tfile, err := ioutil.TempFile(os.TempDir(), "CTdel_var")
+      if err != nil {
+        log.Fatal(err)
+      }
+      count_batches = count_batches + 1
+      if _, err := tfile.Write([]byte("{")); err != nil {
+        log.Fatal(err)
+      }
+      
+      for key2, element2 := range temp_dict {
+        count = count + 1
+        if (count == 1) {
+          temp_string = fmt.Sprintf("\"%s\": %v",key2,element2)
+        } else {
+          temp_string = fmt.Sprintf(",\"%s\": %v",key2,element2)
+        }
+        if _, err := tfile.Write([]byte(temp_string)); err != nil {
+          log.Fatal(err)
+        }
+ 
+      } //end for
+      if _, err := tfile.Write([]byte("}")); err != nil {
+        log.Fatal(err)
+      }
+      tfile.Close()
+      response := ""
+      if *test {
+        fmt.Println("SIMULATION set_variables")
+        response = "NO ACTION"
+      } else {
+        response = del_variables(central_info, tfile.Name())
+      }
+      fmt.Println(response)
+      temp_dict = map[interface{}]interface{}{}
+      fmt.Println(tfile.Name())
+
+     //this will delete the temp file on program exit
+//      defer os.Remove(tfile.Name())
+      fmt.Println("Total devices in file: ", count)
+      count = 1
+    } //endif
+
+  } // end for
+
+  //create temp file
+  tfile, err := ioutil.TempFile(os.TempDir(), "CTdel_var")
+  if err != nil {
+    log.Fatal(err)
+  }
+  count_batches = count_batches + 1
+
+  if _, err := tfile.Write([]byte("{")); err != nil {
+    log.Fatal(err)
+  }
+   
+  for key2, element2 := range temp_dict {
+    count = count + 1
+    if (count == 1) {
+      temp_string = fmt.Sprintf("\"%s\": %v",key2,element2)
+    } else {
+      temp_string = fmt.Sprintf(",\"%s\": %v",key2,element2)
+    }
+    if _, err := tfile.Write([]byte(temp_string)); err != nil {
+      log.Fatal(err)
+    }
+  } 
+  if _, err := tfile.Write([]byte("}")); err != nil {
+    log.Fatal(err)
+  }
+  tfile.Close()
+  fmt.Println("Tmp file name:",tfile.Name())
+  response := ""
+  if *test {
+    fmt.Println("SIMULATION del_variables")
+    response = "NO ACTION"
+  } else {
+    response = del_variables(central_info, tfile.Name())
+  }
+  fmt.Println("Status:",response)
+
+//this will delete the temp file on program exit
+//      defer os.Remove(tfile.Name())
+  fmt.Println("Total devices in file: ", count)
+
+  fmt.Println("Total devices: ", count*count_batches)
+
+//======================================================
+// perform post check
+//======================================================
+//  fmt.Println("--------------------------------")
+//  fmt.Println("Performing Post-Check")
+//  fmt.Println("--------------------------------")
+//  p_check(central_info,*infile,"post-check",*variable)
+   
+}
---
+++ CTsite_serials.go
@@ -0,0 +1,423 @@
+package main
+
+
+import (
+    "fmt"
+    "os"
+    "io/ioutil"
+    "net/http"
+    "time"
+    "strings"
+    "github.com/buger/jsonparser"
+    "github.com/akamensky/argparse"
+    "github.com/micgresham/goCentral"
+)
+
+var appName = "CTsite_serial"
+var appVer = "1.5"
+var appAuthor = "Michael Gresham"
+var appAuthorEmail = "michael.gresham@hpe.com"
+var pgmDescription = fmt.Sprintf("%s: Retrieve a list of device serial numbers for a given site.",appName)
+var central_info goCentral.Central_struct
+var useSecureStorage = true
+
+
+var p_check_dict = map[string]interface{}{}
+
+func get_apSerials(central_info goCentral.Central_struct, siteName string)[]string {
+
+  my_serials := []string{}
+
+  access_token := central_info.Token
+  base_url := central_info.Base_url
+  api_function_url := fmt.Sprintf("%s/monitoring/v2/aps",base_url)
+
+  c := http.Client{Timeout: time.Duration(10) * time.Second}
+  req, err := http.NewRequest("GET", api_function_url, nil)
+  if err != nil {
+      fmt.Printf("error %s", err)
+      return(nil)
+  }
+  q := req.URL.Query()
+  q.Add("site",strings.TrimSpace(siteName))
+  q.Add("offset","0")
+  req.URL.RawQuery = q.Encode()
+ 
+//  fmt.Println(req)
+
+  req.Header.Add("Content-Type", `application/json`)
+  req.Header.Add("Authorization", fmt.Sprintf("Bearer %s",fmt.Sprintf(access_token)))
+  resp, err := c.Do(req)
+  if err != nil {
+      fmt.Printf("error %s", err)
+      return(nil)
+  }
+
+  defer resp.Body.Close()
+  body, err := ioutil.ReadAll(resp.Body)
+//  fmt.Println(resp)
+
+  if err != nil {
+      fmt.Printf("error %s", err)
+      return(nil)
+  }
+  jsonparser.ArrayEach(body, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
+    serial, _ := jsonparser.GetString(value, "serial")
+    my_serials = append(my_serials, serial)
+  }, "aps")
+
+    return(my_serials)
+}
+
+func get_switchSerials(central_info goCentral.Central_struct, siteName string)[]string {
+
+  my_serials := []string{}
+
+  access_token := central_info.Token
+  base_url := central_info.Base_url
+  api_function_url := fmt.Sprintf("%s/monitoring/v1/switches",base_url)
+
+  c := http.Client{Timeout: time.Duration(10) * time.Second}
+  req, err := http.NewRequest("GET", api_function_url, nil)
+  if err != nil {
+      fmt.Printf("error %s", err)
+      return(nil)
+  }
+  q := req.URL.Query()
+  q.Add("site",strings.TrimSpace(siteName))
+  q.Add("offset","0")
+  req.URL.RawQuery = q.Encode()
+ 
+//  fmt.Println(req)
+
+  req.Header.Add("Content-Type", `application/json`)
+  req.Header.Add("Authorization", fmt.Sprintf("Bearer %s",fmt.Sprintf(access_token)))
+  resp, err := c.Do(req)
+  if err != nil {
+      fmt.Printf("error %s", err)
+      return(nil)
+  }
+
+  defer resp.Body.Close()
+  body, err := ioutil.ReadAll(resp.Body)
+//  fmt.Println(resp)
+
+  if err != nil {
+      fmt.Printf("error %s", err)
+      return(nil)
+  }
+  jsonparser.ArrayEach(body, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
+    serial, _ := jsonparser.GetString(value, "serial")
+    my_serials = append(my_serials, serial)
+  }, "switches")
+
+    return(my_serials)
+}
+
+func get_gatewaySerials(central_info goCentral.Central_struct, siteName string)[]string {
+
+  my_serials := []string{}
+
+  access_token := central_info.Token
+  base_url := central_info.Base_url
+  api_function_url := fmt.Sprintf("%s/monitoring/v1/gateways",base_url)
+
+  c := http.Client{Timeout: time.Duration(10) * time.Second}
+  req, err := http.NewRequest("GET", api_function_url, nil)
+  if err != nil {
+      fmt.Printf("error %s", err)
+      return(nil)
+  }
+  q := req.URL.Query()
+  q.Add("site",strings.TrimSpace(siteName))
+  q.Add("offset","0")
+  req.URL.RawQuery = q.Encode()
+ 
+//  fmt.Println(req)
+
+  req.Header.Add("Content-Type", `application/json`)
+  req.Header.Add("Authorization", fmt.Sprintf("Bearer %s",fmt.Sprintf(access_token)))
+  resp, err := c.Do(req)
+  if err != nil {
+      fmt.Printf("error %s", err)
+      return(nil)
+  }
+
+  defer resp.Body.Close()
+  body, err := ioutil.ReadAll(resp.Body)
+//  fmt.Println(resp)
+
+  if err != nil {
+      fmt.Printf("error %s", err)
+      return(nil)
+  }
+  jsonparser.ArrayEach(body, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
+    serial, _ := jsonparser.GetString(value, "serial")
+    my_serials = append(my_serials, serial)
+  }, "gateways")
+
+    return(my_serials)
+}
+
+func get_mcSerials(central_info goCentral.Central_struct, siteName string)[]string {
+
+  my_serials := []string{}
+
+  access_token := central_info.Token
+  base_url := central_info.Base_url
+  api_function_url := fmt.Sprintf("%s/monitoring/v1/mobility_controllers",base_url)
+
+  c := http.Client{Timeout: time.Duration(10) * time.Second}
+  req, err := http.NewRequest("GET", api_function_url, nil)
+  if err != nil {
+      fmt.Printf("error %s", err)
+      return(nil)
+  }
+  q := req.URL.Query()
+  q.Add("site",strings.TrimSpace(siteName))
+  q.Add("offset","0")
+  req.URL.RawQuery = q.Encode()
+ 
+//  fmt.Println(req)
+
+  req.Header.Add("Content-Type", `application/json`)
+  req.Header.Add("Authorization", fmt.Sprintf("Bearer %s",fmt.Sprintf(access_token)))
+  resp, err := c.Do(req)
+  if err != nil {
+      fmt.Printf("error %s", err)
+      return(nil)
+  }
+
+  defer resp.Body.Close()
+  body, err := ioutil.ReadAll(resp.Body)
+//  fmt.Println(resp)
+
+  if err != nil {
+      fmt.Printf("error %s", err)
+      return(nil)
+  }
+  jsonparser.ArrayEach(body, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
+    serial, _ := jsonparser.GetString(value, "serial")
+    my_serials = append(my_serials, serial)
+  }, "mcs")
+
+  return(my_serials)
+}
+
+func doesFileExist(fileName string) bool {
+   _ , error := os.Stat(fileName)
+
+// check if error is "file not exists"
+   if os.IsNotExist(error) {
+//     fmt.Printf("%v file does not exist\n", fileName)
+     return true
+   } else {
+//     fmt.Printf("%v file exist\n", fileName)
+     return false
+   }
+}
+
+func main() {
+
+  parser := argparse.NewParser("CTsite_serials",pgmDescription)
+  token := parser.String("","token", &argparse.Options{Help: "Central API token if not using encrypted storage."})
+  base_url := parser.String("","url", &argparse.Options{Help: "Central API URL if not using encrypted storage."})
+  initDB := parser.Flag("","initDB", &argparse.Options{Help: "Initialize secure storage"})
+
+  siteName := parser.String("","site", &argparse.Options{Help: "Target site name"})
+  devType := parser.String("","dev_type", &argparse.Options{Help: "Device type requested: ALL|AP|SW|GW|MC",Required: false})
+  config := parser.String("c", "config", &argparse.Options{Help: "Config file location"})
+
+  ap_serials := []string{}
+  sw_serials := []string{}
+  gw_serials := []string{}
+  mc_serials := []string{}
+
+  //encrypted storage setup
+  SSfilename:= "CTconfig.yml"
+  goCentral.Passphrase = "“You can use logic to justify almost anything. That’s its power. And its flaw. –Captain Cathryn Janeway"
+
+  fmt.Println("-------------------------------------")
+  fmt.Printf("%s Version: %s\r\n",appName, appVer)
+  fmt.Printf("Author: %s (%s)\r\n",appAuthor, appAuthorEmail)
+  fmt.Println("-------------------------------------")
+
+
+  err := parser.Parse(os.Args)
+  if err != nil {
+        fmt.Println(parser.Usage(err))
+        return
+  }
+
+  if *config != "" {
+    SSfilename = *config
+    fmt.Println("-------------------------------------")
+    fmt.Println("Loading config file from ",SSfilename)
+    fmt.Println("-------------------------------------")
+  }
+
+  //initialize the secure storage if requested
+  if *initDB {
+     goCentral.Init_DB(SSfilename)
+     os.Exit(0) //we do not do anything after the secure storage initialization
+  }
+
+
+  if doesFileExist(SSfilename) {
+
+     //we are not using secure storage
+     useSecureStorage = false
+
+     //if the user provided a token AND a URL we will use it
+     if (*token != "") {
+        if (*base_url == "") {
+           fmt.Println("Token supplied, but Central URL is missing.  Both are required if using the command line options.")
+           os.Exit(1)
+        }
+     } else { //ask for the token
+
+     fmt.Print("\nProvide the Central API URL: ")
+     fmt.Scanln(base_url)
+     fmt.Print("Provide the Central token: ")
+     fmt.Scanln(token)
+
+     central_info.Base_url = *base_url
+     central_info.Customer_id = ""
+     central_info.Client_id = ""
+     central_info.Client_secret = ""
+     central_info.Token = *token
+     central_info.Refresh_token = ""
+
+     }
+  } else {
+    fmt.Println("Reading secure storage")
+
+    central_info = goCentral.Read_DB(SSfilename)
+    fmt.Printf("---------------------------\n")
+    fmt.Printf("Central Info Decrypted\n")
+    fmt.Printf("---------------------------\n")
+    fmt.Printf("Central URL: %s\n",central_info.Base_url)
+//    fmt.Printf("Central Customer ID: %s\n",central_info.Customer_id)
+//    fmt.Printf("Central Client ID: %s\n",central_info.Client_id)
+//    fmt.Printf("Central Client Secret: %s\n",central_info.Client_secret)
+    fmt.Printf("Central Token: %s\n",central_info.Token)
+//    fmt.Printf("Central Refresh Token: %s\n",central_info.Refresh_token)
+  }
+
+
+
+ if (*siteName == "") {
+    fmt.Print("\nProvide the site name: ")
+    fmt.Scanln(siteName)
+ }
+
+ if (*devType == "") {
+   fmt.Print("\nProvide the device type requested (ALL|AP|SW|GW|MC): ")
+   fmt.Scanln(devType)
+ }
+  fmt.Println("Target site name:",*siteName)
+  fmt.Println("Requested device type:",*devType)
+
+//======================================================
+// test if valid token
+//======================================================
+  respCode, new_token, new_refresh_token := goCentral.Test_central(central_info)
+  if (respCode != 200) {
+     fmt.Printf("\nCentral access failed with response code: %d\n",respCode)
+     os.Exit(3)
+  } else {
+     fmt.Print("Central access OK.  Token verified.")
+     fmt.Printf("Response code: %d\n",respCode)
+     central_info.Token = new_token
+     if useSecureStorage {
+       central_info.Refresh_token = new_refresh_token
+       goCentral.Write_DB(SSfilename,central_info)
+     }
+  }
+  if (respCode != 200) {
+   fmt.Printf("\nCentral access failed with response code: %d\n",respCode)
+   os.Exit(3)
+  }
+
+
+//---------------------------------------
+//  os.Exit(0)
+//---------------------------------------
+
+
+//prepare the output file
+  ofilename := fmt.Sprintf("%s-serials.txt",*siteName)
+  fmt.Printf("\nOutput file written to %s\n\n",ofilename)
+  of, err := os.Create(ofilename)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+  if (*devType == "ALL") {
+    fmt.Print("APs: ")
+    ap_serials = get_apSerials(central_info, *siteName)
+    fmt.Println(ap_serials)
+
+    fmt.Print("Switches: ")
+    sw_serials = get_switchSerials(central_info, *siteName)
+    fmt.Println(sw_serials)
+
+    fmt.Print("Gateways: ")
+    gw_serials = get_gatewaySerials(central_info, *siteName)
+    fmt.Println(gw_serials)
+
+    fmt.Print("Mobility Controllers: ")
+    mc_serials = get_mcSerials(central_info, *siteName)
+    fmt.Println(mc_serials)
+   } else if (*devType == "AP") {
+    fmt.Print("APs: ")
+    ap_serials = get_apSerials(central_info, *siteName)
+    fmt.Println(ap_serials)
+   }  else if (*devType == "SW") {
+    fmt.Print("Switches: ")
+    sw_serials = get_switchSerials(central_info, *siteName)
+    fmt.Println(sw_serials)
+   }  else if (*devType == "GW") {
+    fmt.Print("Gateways: ")
+    gw_serials = get_gatewaySerials(central_info, *siteName)
+    fmt.Println(gw_serials)
+   }  else if (*devType == "MC") {
+    fmt.Print("Mobility Controllers: ")
+    mc_serials = get_mcSerials(central_info, *siteName)
+   }
+   
+  for _, serial := range ap_serials {
+        _, err := of.WriteString(serial + "\n")
+        if err != nil {
+          fmt.Println(err)
+          return
+        }
+    }
+  for _, serial := range sw_serials {
+        _, err := of.WriteString(serial + "\n")
+        if err != nil {
+          fmt.Println(err)
+          return
+        }
+    }
+  for _, serial := range gw_serials {
+        _, err := of.WriteString(serial + "\n")
+        if err != nil {
+          fmt.Println(err)
+          return
+        }
+    }
+  for _, serial := range mc_serials {
+        _, err := of.WriteString(serial + "\n")
+        if err != nil {
+          fmt.Println(err)
+          return
+        }
+  }
+  err = of.Close()
+  if err != nil {
+     fmt.Println(err)
+     return
+  }
+}
